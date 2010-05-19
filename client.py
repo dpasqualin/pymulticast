b9e7c0e8cb43ea75f41f63618164aebcdb480bb4
Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>
2010-05-19 11:18:00 -0300

    client.py: Cliente feito (na teoria pelo menos)

--- client.py
+++ client.py
@@ -1,16 +1,46 @@
 #!/usr/bin/env python
 
 from mcastcalculator import McastCalculatorClient
-import sys
+import sys,re
 
 class OnlineCalc(McastCalculatorClient):
-    pass
+    def run(self,request):
+        exp = "^[0-9()\+\-\/\*]*$"
+        if re.match(exp,request):
+            return McastCalculatorClient.run(self,request)
+        else:
+            return None
 
 def usage():
-    print "./%s mcastPort mcastAddr udpPort udpAddr" % sys.argv[0]
+    print "%s mcastPort mcastAddr udpPort udpAddr" % sys.argv[0]
     print "\tUm prompt informa que os calculos ja podem ser digitados."
     print "\tUm <enter> separa cada calculo."
 
-if __name__ == "__main__":
-    pass
+def main(argc, argv):
+    if argc != 5:
+        usage()
+        sys.exit(1)
 
+    # Argumentos
+    mcastPort,mcastAddr,udpPort,udpAddr = argv[1:]
+
+    # Cria conexao com o multicast e abre udp para resposta
+    onlinecalc = OnlineCalc(mcastPort,mcastAddr,udpPort,udpAddr)
+
+    # Loop para leitura de comandos
+    print "ctrl+c para sair."
+    while True:
+        try:
+            calc = raw_input("> ")
+            print onlinecalc.run(calc)
+        except (EOFError,KeyboardInterrupt):
+            print ""
+            return 0
+
+    return 0
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main(len(sys.argv),sys.argv))
+    except:
+        raise
