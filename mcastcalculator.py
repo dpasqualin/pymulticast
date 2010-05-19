494eadab9c2603ad540b6530ad754d742767ac6c
Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>
2010-05-19 08:17:16 -0300

    mcastcalculator: Implementada classe McastCalculatorServer

--- mcastcalculator.py
+++ mcastcalculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-from mcast import McastClient
-from udp import UDPServer
+from mcast import McastClient,McastServer
+from udp import UDPServer,UDPClient
 import sys,os
 
 ANY = "0.0.0.0"
@@ -12,16 +12,39 @@ UDP_PORT = 20000
 TTL = 1 # Nao sera repassado a nenhum router
 UDP_HOST="localhost"
 
+class McastCalculatorServer(object):
+    """ Esta classe eh responsavel por ler uma nova requisicao de calculo do
+    servico multicast e posteriormente enviar uma resposta ao cliente  """
+    def __init__(self, mcastPort, mcastAddr):
+
+        # Recebe requisicoes do multicast
+        self.__mcastSrv = McastServer(mcastPort,mcastAddr)
+
+    def getRequest(self):
+        """ Le uma nova requisicao do multicast """
+        return self.getMcast().read()
+
+    def sendResponse(self,udpHost,udpPort,response):
+        """ Envia response para client (udpHost,udpPort) """
+        UDPClient(udpHost,udpPort).send(response)
+
+    def getMcast(self):
+        return self.__mcastSrv
 
 class McastCalculatorClient(object):
+    """ Essa classe envia uma requisicao de calculo para
+    (mcastPort,mcastAddr) e aguarda resposta em (udpHost,udpPort) """
     def __init__(self, mcastPort, mcastAddr, udpHost, udpPort):
         # Sera usado para enviar requisicoes ao servico multicast
-        self.__mcastClt = McastClient(MCAST_PORT,MCAST_ADDR)
+        self.__mcastClt = McastClient(mcastPort,mcastAddr)
 
         # O Socket udp aguarda pela resposta dos servidores
-        self.__udpClt = UDPServer(UDP_HOST,UDP_PORT)
+        self.__udpSrv = UDPServer(udpHost,udpPort)
 
     def calculate(self, calc):
+        """ Envia equacao "calc" via multicast e aguarda resposta via udp.
+            calc eh uma string que pode possuir os simbolos +-/*()
+        """
         self.__sendToMcast(calc)
         return self.readFromUDP()
 
@@ -35,5 +58,5 @@ class McastCalculatorClient(object):
         return self.__mcastClt
 
     def getUDP(self):
-        return self.__udpClt
+        return self.__udpSrv
 
