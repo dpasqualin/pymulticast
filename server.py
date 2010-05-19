d78bb587c40cf32f3838ce16189ccfb80b4f5276
Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>
2010-05-19 16:39:46 -0300

    server.py: Criada funcao run() (e outras) que servira como parser

--- server.py
+++ server.py
@@ -1,17 +1,63 @@
 #!/usr/bin/env python
 
 from mcastcservice import McastServiceServer
-import re,sys,socket,Thread
+import re,sys,socket,threading
 from misc import Server,Request
 
-class OnlineCalcServer(McastServiceServer):
+class OnlineCalcServer(McastServiceServer,threading.Thread):
     """ serverDict: dicionario com elementos Server """
     def __init__(self, severId, mcastPort, mcastAddr, serverDict):
 
+        threading.Thread.__init__(self)
         McastServiceServer.__init__(self,mcastPort,mcastAddr)
         self.__serverDict = serverDict
         self.__server = serverId
         self.__requestList = []
+
+        # Sinaliza quando a thread tera que ser fechada
+        self.__quit = False
+
+    def run(self):
+        """ Este metodo recebe as mensagens e endereca para o metodo
+        correto """
+
+        # Expressoes regulares de possiveis mensagens
+        reID = "(?P<id>[0-9]+)"
+        reREQCONF = "(?P<request>.*)"
+        reALIVE = re.compile("^%s:ALIVE$" % reID)
+        reCONFIRM = re.compile("%s:CONFIRM:%s$" % (reID,reREQCONF))
+        reREQUEST = re.compile("^(?P<request>[0-9()\+\-\/\*]*)$")
+
+        while not self.__quit:
+            ((port,host),data) = getRequest()
+            if reALIVE.match(data):
+                data = reALIVE.search(data)
+                self.hearBeatReceived(int(data.group("id")))
+            elif reCONFIRM.match(data):
+                data = reCONFIRM.search(data)
+                serverID = data.group("id")
+                request = data.group("request")
+                self.requestSentby(serverId,request)
+            elif reREQUEST.match(data):
+                data = reREQUEST.search(data)
+                request = Request(data.group("request"))
+                self.addRequest(request)
+
+    def addRequest(self, request):
+        """ Adiciona uma nova requisicao a lista e a trata """
+
+        # TODO: tratar request
+        self.requestList.append(request)
+
+    def removeRequest(self, request):
+        """ Remove request da lista """
+        if request in self.__requestList:
+            self.__requestList.remove(request)
+
+    def requestSentby(self, serverID, request):
+    """ Confirmacao de que o servidor serverID respondeu a requisicao
+    request, que deve ser removida da lista """
+        self.removeRequest(request)
 
     def heartBeatReceived(self,serverID):
         """ Marca servidor serverID como vivo """
