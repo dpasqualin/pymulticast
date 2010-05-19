50f194ea5818bb54e2668c79ebe86c957be81449
Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>
2010-05-18 22:15:42 -0300

    udp.py: Adicionada interface para comunicacao UDP
    Sera usada para o servidor multicast responder a requisicao do client

---
+++ udp.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from socket import *
+
+class UDPParams(object):
+
+    def __init__(self,host,port):
+        self.__socket = socket(AF_INET,SOCK_DGRAM)
+        self.__host = host
+        self.__port = port
+        self.__addr = (self.getHost(),self.getPort())
+
+    def send(self, msg):
+        self.getSocket().sendto(msg,self.getAddr())
+
+    def read(self):
+        return self.getSocket().recvfrom(1024)
+
+    def getSocket(self):
+        return self.__socket
+
+    def getPort(self):
+        return self.__port
+
+    def getHost(self):
+        return self.__host
+
+    def getAddr(self):
+        return self.__addr
+
+class UDPServer(UDPParams):
+
+    def __init__(self,host,port):
+        UDPParams.__init__(self,host,port)
+        self.__connect()
+
+    def __connect(self):
+        print "sou server"
+        self.getSocket().bind(self.getAddr())
+
+class UDPClient(UDPParams):
+    pass
