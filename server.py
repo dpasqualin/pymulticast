613c3c02a27f84b1e9077f26c2f06f0c5fe142c6
Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>
2010-05-24 10:35:41 -0300

    server: Adicionadas chamadas para gravar mensagens em log

--- server.py
+++ server.py
@@ -2,7 +2,8 @@
 
 from mcastservice import McastServiceServer
 import re,sys,socket,threading
-from misc import Server,Request,Timeout
+from misc import Server,Request,Timeout,Log
+from misc import LOGERROR,LOGWARNING,LOGCONTROL,LOGMESSAGE,LOGDEBUG
 
 # timeout do heartbeat, em segundos
 TOUT_HEARTBEAT = 5.0
@@ -16,6 +17,8 @@ class OnlineCalcServer(McastServiceServer,threading.Thread):
         self.__server = int(serverId)
         self.__requestList = []
         self.__timeoutList = []
+        self.__log = Log()
+        self.writeLog(LOGCONTROL,"Iniciando...")
 
         # Sinaliza quando a thread tera que ser fechada
         self.__quit = False
@@ -38,10 +41,10 @@ class OnlineCalcServer(McastServiceServer,threading.Thread):
 
         while not self.__quit:
             (data,(ip,port)) = self.getRequest()
-            print data,ip,port
             if reALIVE.match(data):
                 data = reALIVE.search(data)
-                self.heartBeatReceived(int(data.group("id")))
+                sid = int(data.group("id"))
+                self.heartBeatReceived(sid)
             elif reCONFIRM.match(data):
                 data = reCONFIRM.search(data)
                 serverID = data.group("id")
@@ -66,18 +69,24 @@ class OnlineCalcServer(McastServiceServer,threading.Thread):
 
         return serverList
 
+    def writeLog(self,verbose,msg):
+        msg = "Server-%d %s"%( self.getServer().getID(),msg )
+        self.__log.log(verbose,msg)
+
     def addTimeout(self,obj):
         self.__timeoutList.append(obj)
 
     def addRequest(self, request):
         """ Adiciona uma nova requisicao a lista e a trata """
 
+        self.writeLog(LOGMESSAGE,"Request:%s"%request)
         # TODO: tratar request
         self.requestList.append(request)
 
     def removeRequest(self, request):
         """ Remove request da lista """
         if request in self.__requestList:
+            self.writeLog(LOGMESSAGE,"Request processado:%s"%request)
             self.__requestList.remove(request)
 
     def requestSentby(self, serverID, request):
@@ -87,14 +96,17 @@ class OnlineCalcServer(McastServiceServer,threading.Thread):
 
     def heartBeatReceived(self,serverID):
         """ Marca servidor serverID como vivo """
+        self.writeLog(LOGCONTROL,"HeartBeatReceived:%d"%serverID)
         self.getServerDict()[int(serverID)].setNotAlive()
 
     def missingHeartBeat(self,serverID):
         """ Marca servidor serverID como morto """
+        self.writeLog(LOGCONTROL,"MissingHeartBeat:%d"%serverID)
         self.getServerDict()[int(serverID)].setAlive()
 
     def sendHeartBeat(self):
         """ Envia mensagem ao multicast informando que esta vivo """
+        self.writeLog(LOGCONTROL,"HeartBeatSent")
         self.getMcast().send("%d:ALIVE"%self.getServer().getID())
 
     def whoAnswers(self):
@@ -110,6 +122,7 @@ class OnlineCalcServer(McastServiceServer,threading.Thread):
         requisicao request foi respondida. """
         if self.whoAnswers() == self.getServer():
             reply = eval(request.getRequest())
+            self.writeLog(LOGCONTROL,"Respondendo %s = %s"%(request,reply))
             host,port = request.getHost(),request.getPort()
             McastServiceServer.sendReply(self,host,port,reply)
             self.sendReplyConfirm(request)
@@ -131,5 +144,7 @@ class OnlineCalcServer(McastServiceServer,threading.Thread):
 
     def quit(self):
         self.__quit = True
+        self.writeLog(LOGCONTROL,"Saindo")
         for tout in self.getTimeoutList():
             tout.quit()
+        self.writeLog(LOGDEBUG,"Morto")
